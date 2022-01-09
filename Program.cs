cb11cb1d6965900958016ae278dfdd15274947e5
Sukalyan <sukalyandash1920@gmail.com>
2022-01-09 23:43:54 +0530

    Snake And Ladder programs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnakeAndLadderProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder program");
+            //UC1_PositionAtZero.playerAtPositionZero();
+            //UC2_RollTheDice.RollTheDice();
+            //UC3_CheckingPosition.CheckingPosition();
+            //UC4_RepeatTillWin.RepeatTillWin();
+            //UC5_RepeatTillWinningPosition.RepeatTillWinningPosition();
+            UC6_CountRollDice.CountRollDice();
+
+        }
+    }
+}
---
+++ UC1_PositionAtZero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadderProblem
+{
+    class UC1_PositionAtZero
+    {
+        public static void playerAtPositionZero()
+        {
+            int player = 1;
+            int startingPosition = 0;
+            Console.WriteLine("Number of players=" + player);
+            Console.WriteLine("position at start=" + startingPosition);
+
+        }
+
+    }
+}
